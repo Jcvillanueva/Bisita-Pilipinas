4f3a66467201d3e286f5f427f86b1022d8db68ed
John Paul Villanueva <Jcvillanueva@student.apc.edu.ph>
2022-11-05 17:32:49 +0800

    Update Tour_Spots_p3.php

--- bisita-pilipinas/Tour_Spots_p3.php
+++ bisita-pilipinas/Tour_Spots_p3.php
@@ -147,7 +147,16 @@ session_start();
                     </div>
                     <div class="col-md-5">
 
-                    <div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=500&amp;height=500&amp;hl=en&amp;q=calo rd nueva&amp;t=k&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://formatjson.org/">format json</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:500px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:500px;}.gmap_iframe {height:500px!important;}</style></div>
+                    <div class="mapouter">
+                      <div class="gmap_canvas">
+                        <iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=500&amp;height=500&amp;hl=en&amp;q=Phillipine carabao centre farm carranglan Nueva ecija&amp;t=k&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://formatjson.org/">format json</a>
+                        </div>
+                        <style>
+                        .mapouter{position:relative;text-align:right;width:100%;height:500px;}
+                        .gmap_canvas {overflow:hidden;background:none!important;width:100%;height:500px;}
+                        .gmap_iframe {height:500px!important;}
+                        </style>
+                        </div>
                     </div>
                     </div>
 
