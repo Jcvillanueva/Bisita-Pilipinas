9120b150eae74b486796f88e1d541b07bd5b0173
John Paul Villanueva <Jcvillanueva@student.apc.edu.ph>
2022-11-06 11:12:45 +0800

    Updated UI of Owner BnB

--- bisita-pilipinas/OwnerBnBhomepage.php
+++ bisita-pilipinas/OwnerBnBhomepage.php
@@ -25,7 +25,7 @@ session_start();
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
       <link rel="stylesheet" href="CSS/Index.css">
       <link rel="stylesheet" href="CSS/headers.css">
-      <link rel="stylesheet" href="CSS/card.css">
+      <!-- <link rel="stylesheet" href="CSS/card.css"> -->
       <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
       <!-- <link rel="stylesheet" href="CSS/sidebars.css"> -->
       <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sidebars/">
@@ -64,11 +64,11 @@ session_start();
              <span class="tooltip">Search</span>
           </li>
           <li>
-            <a href="OwnerHomepage.html">
+            <a href="OwnerBnBhomepage.php">
               <i class='bx bx-grid-alt'></i>
               <span class="links_name">Your BnB</span>
             </a>
-             <span class="tooltip">List of BnB</span>
+             <span class="tooltip">Check Your BnB</span>
           </li>
          <li>
            <a href="Tour_Spots_Owner.php">
@@ -88,123 +88,57 @@ session_start();
          </li>
         </ul>
       </div>
+      <?php
+                while($row = mysqli_fetch_assoc($result)) {
+                ?>
       <!-- Home-section -->
       <section class="home-section">
-        <div class="text">Your BnB<div class="">
-        <?php
-          while($row = mysqli_fetch_assoc($result)) {
-            
-        ?>
-        <div class="row">
-            <!-- House #1 -->
-            <h2><?php echo $row['owner_bnb_name']; ?></h2>
-            <div class="fakeimg" style="height:200px;">
-
-                    <div class="homepage_row"> 
-                      <?php echo '<img src= "./images/'.$row['owner_bnb_image'].'" 
-                         style="width:192;height:144px;margin:5px;border: 2px solid #000000;float:left;"/>';  ?>
-
-                      <div class="homepageColumn"> 
-                        <?php echo '<img src= "./images/'.$row['owner_bnb_image2'].'" 
-                          style="width:192px;height:144px;margin:5px;border: 2px solid #000000;float:left;"/>';  ?>
-                      </div>
-
-                      <div class="homepageColumn"> 
-                        <?php echo '<img src= "./images/'.$row['owner_bnb_image3'].'" 
-                          style="width:192px;height:144px;margin:5px;border: 2px solid #000000;float:left;"/>';  ?>
-                      </div>
-
-                      <div class="homepageColumn"> 
-                        <?php echo '<img src= "./images/'.$row['owner_bnb_image4'].'" 
-                          style="width:192px;height:144px;margin:5px;border: 2px solid #000000;float:left;"/>';  ?>
-                      </div>
-                      <div class="homepageColumn"> 
-                        <?php echo '<img src= "./images/'.$row['owner_bnb_image5'].'" 
-                          style="width:192px;height:144px;margin:5px;border: 2px solid #000000;float:left;"/>';  ?>
-                      </div>
-                      
-                    </div>
-                              
-                  </div>
-                        
-            <br>
-            <h5><?php echo $row['owner_bnb_desc']; ?> </h5>
-            <!-- First Accordion house -->
-            <div class="accordion" id="accordionExample1">
-              <div class="accordion-item">
-                <h2 class="accordion-header" id="headingOne">
-                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    More details
-                  </button>
-                </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                  <div class="accordion-body">
-                    <strong>Contact Number: <?php echo $row['owner_contact_num']; ?> </strong><br> 
-                    <strong>Price Range: <?php echo $row['price_range_from']; ?> - <?php echo $row['price_range_to']; ?> </strong>
-                  </div>
-                </div>
-              </div>
-              <div class="accordion-item">
-                <h2 class="accordion-header" id="headingTwo">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    Location
-                  </button>
-                </h2>
-                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                  <div class="accordion-body">
-                    <strong><?php echo $row['owner_bnb_address']; ?></strong> 
-                  </div>
-                </div>
-              </div>
-              <!-- <div class="accordion-item">
-                <h2 class="accordion-header" id="headingThree">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Ratings 
-                  </button>
-                </h2>
-                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                  <div class="accordion-body">
-                    <strong>in progress pa ren to pero magagawa ko na to agaad in no time</strong>
-                  </div>
-                </div>
-              </div> -->
+      <div class="text"> Your BnB</div>
+      <div class="container">
+      <div class="card mb-3" style="max-width: 720px; max-height:920px;">
+          <div class="row no-gutters">
+            <div class="col-md-3">
+            <?php echo '<img src= "./images/'.$row['owner_bnb_image'].'" 
+           style="width:192px; height:144px; margin:5px; border: 2px solid #000;"/>';  ?>
             </div>
-            <!-- First Accordion House End -->
-            <!-- House #1 End -->
-            <!--
-            <br>
-            <br>
-            <br>
-              <div class="pagination_section">
-                <nav aria-label="...">
-                  <ul class="pagination">          
-                      <a class="page-link" href="homepage.html" tabindex="-1" aria-disabled="false">Previous</a>
-                    <li class="page-item active" aria-current="page">
-                      <a class="page-link" href="homepage.html">1</a></li>
-                    <li class="page-item">
-                      <a class="page-link" href="homeP2.html">2</a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="homeP3.html">3</a></li>
-                    <li class="page-item">
-                      <a class="page-link" href="homeP2.html">Next</a>
-                    </li>
-                  </ul>
-                </nav>
+            <div class="col-md-9">
+              <div class="card-body">
+                <h5 class="card-title"><strong>Owner:</strong><?php echo $row['owner_bnb_name']; ?></h5>
+                <p class="card-text"><strong>Address:</strong><?php echo $row['owner_bnb_address'];?></p>
+                <p class="card-text"><strong>Description:</strong><?php echo $row['owner_bnb_desc'];?></p>
+                <p class="card-text"><strong>Contact Number:</strong><?php echo $row['owner_contact_num'];?></p>
+                <a href="#" class="btn btn-primary" style="color: #fff; background-color:#11101D; border-color:#11101D;">View Details</a>
               </div>
-            </div> 
-            -->
-          
+            </div>
+          </div>
+        </div>       
+        <div class="card mb-3" style="max-width: 720px; max-height:920px;">
+          <div class="row no-gutters">
+            <div class="col-md-3">
+            <?php echo '<img src= "./images/'.$row['owner_bnb_image'].'" 
+           style="width:192px; height:144px; margin:5px; border: 2px solid #000;"/>';  ?>
+            </div>
+            <div class="col-md-9">
+              <div class="card-body">
+                <h5 class="card-title"><strong>Owner:</strong><?php echo $row['owner_bnb_name']; ?></h5>
+                <p class="card-text"><strong>Address:</strong><?php echo $row['owner_bnb_address'];?></p>
+                <p class="card-text"><strong>Description:</strong><?php echo $row['owner_bnb_desc'];?></p>
+                <p class="card-text"><strong>Contact Number:</strong><?php echo $row['owner_contact_num'];?></p>
+                <a href="#" class="btn btn-primary" style="color: #fff; background-color:#11101D; border-color:#11101D;">View Details</a>
+              </div>
+            </div>
+          </div>
         </div>
-        <hr>
+       
+    </div> 
+
         <?php
           }
-        ?>
-<?php
- }
-?>
-        </div>
-    </div>
-    </section>
+          ?>
+  <?php
+    }
+    ?>
+      </section>
     <!-- Home-section end -->
 
       <script>
